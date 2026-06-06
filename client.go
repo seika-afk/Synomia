52437d5602714ca909fec75fcbc1b3b41105a7f1
_Gagan <84296830+seika-afk@users.noreply.github.com>
2026-06-06 17:06:11 +0530

    Update websocket CheckOrigin to allow all origins
    Modify CheckOrigin function to allow all origins.

--- client.go
+++ client.go
@@ -32,8 +32,8 @@ var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 	CheckOrigin: func(r *http.Request) bool {
-		origin := r.Header.Get("Origin")
-		return origin == "http://localhost:3000" || origin == ""
+
+		return true
 	},
 }
 
